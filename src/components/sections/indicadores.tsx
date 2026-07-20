eb57d6e7b09058ec16e8cfb1b7cc7525261d2324
gabrielluks.dev <gabrielluks.dev>
2026-07-19 23:15:41 -0300

    Colocando no Vercel

--- src/components/sections/indicadores.tsx
+++ src/components/sections/indicadores.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { motion, Variants } from "framer-motion";
 import { Briefcase, Star, Clock, ShieldCheck } from "lucide-react";
 
 const stats = [
@@ -30,7 +30,7 @@ const stats = [
   }
 ];
 
-const containerVariants = {
+const containerVariants: Variants = {
   hidden: { opacity: 0 },
   visible: {
     opacity: 1,
@@ -40,7 +40,7 @@ const containerVariants = {
   }
 };
 
-const itemVariants = {
+const itemVariants: Variants = {
   hidden: { opacity: 0, y: 30 },
   visible: { 
     opacity: 1, 
